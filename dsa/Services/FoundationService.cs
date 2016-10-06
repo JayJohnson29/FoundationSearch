6b76f4bcddf55a7c543a3f61aa3c91e6f40e3eae
Jay Johnson <jay.johnson@connectedintouch.com>
2016-10-06 09:43:15 -0600

    Add Export Buttons

--- dsa/Services/FoundationService.cs
+++ dsa/Services/FoundationService.cs
@@ -19,78 +19,24 @@ namespace dsa.Services
             public string Accredited { get; set; }
             public string Operator { get; set; }
         }
-        public List<Models.FoundationModel> GetFoundationsSql(CouncilFoundationModel model)
-        {
-            //new SelectListItem { Text = "N/A", Value = "0" },
-            //new SelectListItem { Text = "Member", Value = "1" },
-            //new SelectListItem { Text = "Accredited", Value = "2" },
-            //new SelectListItem { Text = "Member and Accredited", Value = "3" },
-            //new SelectListItem { Text = "Member or Accredited", Value = "4" }
 
-            CouncilStatus status;
-            switch (model.CouncilStatus)
-            {
-                case "0":
-                    status = new CouncilStatus { Accredited = "", Member = "", Operator = "" };
-                    break;
-                case "1":
-                    status = new CouncilStatus { Accredited = "NO", Member = "YES", Operator = "AND" };
-                    break;
-                case "2":
-                    status = new CouncilStatus { Accredited = "YES", Member = "NO", Operator = "AND" };
-                    break;
-                case "3":
-                    status = new CouncilStatus { Accredited = "YES", Member = "YES", Operator = "AND" };
-                    break;
-                case "4":
-                    status = new CouncilStatus { Accredited = "YES", Member = "YES", Operator = "OR" };
-                    break;
-                default:
-                    status = new CouncilStatus { Accredited = "", Member = "", Operator = "" };
-                    break;
-            }
-
-            string congressional = string.Empty;
-
-            if (!string.IsNullOrEmpty(model.District))
-            {
-                char[] arr = model.District.Where(c => (char.IsLetterOrDigit(c) || char.IsWhiteSpace(c))).ToArray();
-                congressional = new string(arr);
-            }
-
-
-            using (var ctx = new ApplicationDbContext())
-            {
-                var state = new SqlParameter("@inpSTATE", model.State ?? string.Empty);
-                var cong = new SqlParameter("@inpCongressional", congressional);
-                var mem = new SqlParameter("@inpMEMFLAG", status.Member);
-                var acc = new SqlParameter("@inpAccredited", status.Accredited);
-                var opp = new SqlParameter("@inpOperator", status.Operator);
-
-                var x = ctx.Database.SqlQuery<FoundationResponseModel>("USR_COF_CONGRESSIOAL_DISTRICT_SP @inpSTATE, @inpCongressional, @inpMEMFLAG, @inpAccredited, @inpOperator", state, cong, mem, acc, opp);
-                return x.Select(a => new FoundationModel { Id = a.ORG_ID, OrgName = a.ORG_NAME, District = a.CONGRESSIONAL, CFNSB = a.ORG_COF_STATUS, CouncilStatus = a.MEMSTATUS, Address1 = a.ADDRESS_1, Address2 = a.ADDRESS_2, City = a.City, State = a.State }).ToList();
-                // return x.ToList();
-            }
-        }
-
-        public List<KeyValuePair<string, string>> GetAllStates1()
-        {
-            using (var ctx = new ApplicationDbContext())
-            {
-                var x = ctx.Database.SqlQuery<string>("USR_COF_CONGRESSIOAL_DISTRICT_STATES_SP");
-                return x.Select(a => new KeyValuePair<string, string>(a, a)).ToList();
-            }
-        }
         public List<Models.FoundationModel> GetFoundations(CouncilFoundationModel model)
         {
             try
             {
+                PersonifyInitializer.Initialize(".\\", new ImmediateSeatInformationProvider());
+                IStoredProcedureRequest storedProcedureRequest = new StoredProcedureRequest("USR_COF_CONGRESSIONAL_DISTRICT_SP");
 
-                CouncilStatus status;
+                IStoredProcedureParameterItems invocationParameters = storedProcedureRequest.InvocationParameters;
+
+                invocationParameters.Add(new StoredProcedureParameterItem("inpSTATE", model.State ?? string.Empty, ParameterDirection.Input));
+                invocationParameters.Add(new StoredProcedureParameterItem("inpCongressional", model.District, ParameterDirection.Input));
+
+                CouncilStatus status = null;
                 switch (model.CouncilStatus)
                 {
                     case "0":
-                        status = new CouncilStatus { Accredited = "", Member = "", Operator = "" };
+                        invocationParameters.Add(new StoredProcedureParameterItem("inpOperator", "OR", ParameterDirection.Input));
                         break;
                     case "1":
                         status = new CouncilStatus { Accredited = "NO", Member = "YES", Operator = "AND" };
@@ -105,29 +51,16 @@ namespace dsa.Services
                         status = new CouncilStatus { Accredited = "YES", Member = "YES", Operator = "OR" };
                         break;
                     default:
-                        status = new CouncilStatus { Accredited = "", Member = "", Operator = "" };
+                        invocationParameters.Add(new StoredProcedureParameterItem("inpOperator", "OR", ParameterDirection.Input));
                         break;
                 }
 
-                string congressional = string.Empty;
-
-                if (!string.IsNullOrEmpty(model.District))
+                if (status != null)
                 {
-                    char[] arr = model.District.Where(c => (char.IsLetterOrDigit(c) || char.IsWhiteSpace(c))).ToArray();
-                    congressional = new string(arr);
+                    invocationParameters.Add(new StoredProcedureParameterItem("inpMEMFLAG", status.Member, ParameterDirection.Input));
+                    invocationParameters.Add(new StoredProcedureParameterItem("inpAccredited", status.Accredited, ParameterDirection.Input));
+                    invocationParameters.Add(new StoredProcedureParameterItem("inpOperator", status.Operator, ParameterDirection.Input));
                 }
-
-
-                PersonifyInitializer.Initialize(".\\", new ImmediateSeatInformationProvider());
-                IStoredProcedureRequest storedProcedureRequest = new StoredProcedureRequest("USR_COF_CONGRESSIONAL_DISTRICT_SP");
-
-                IStoredProcedureParameterItems invocationParameters = storedProcedureRequest.InvocationParameters;
-
-                invocationParameters.Add(new StoredProcedureParameterItem("inpSTATE", model.State ?? string.Empty, ParameterDirection.Input));
-                invocationParameters.Add(new StoredProcedureParameterItem("inpCongressional", congressional, ParameterDirection.Input));
-                invocationParameters.Add(new StoredProcedureParameterItem("inpMEMFLAG", status.Member, ParameterDirection.Input));
-                invocationParameters.Add(new StoredProcedureParameterItem("inpAccredited", status.Accredited, ParameterDirection.Input));
-                invocationParameters.Add(new StoredProcedureParameterItem("inpOperator", status.Operator, ParameterDirection.Input));
 
                 IQueryRequest request = new QueryRequest(storedProcedureRequest);
                 IQueryResult queryResult = Global.App.Execute(request);
@@ -168,8 +101,8 @@ namespace dsa.Services
                             Id = id,
                             OrgName = dt.Rows[i][1].ToString(),
                             District = dt.Rows[i][5].ToString(),
-                            CFNSB = dt.Rows[i][3].ToString(),
-                            CouncilStatus = dt.Rows[i][2].ToString(),
+                            CFNSB = dt.Rows[i][4].ToString(),
+                            CouncilStatus = dt.Rows[i][3].ToString(),
                             Address1 = dt.Rows[i][6].ToString(),
                             Address2 = dt.Rows[i][7].ToString(),
                             City = dt.Rows[i][9].ToString(),
